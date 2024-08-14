9f85287f1462bb8ce636243a6945443e59bcb7f5
BIThovenKostyukevich <153630776+BIThovenKostyukevich@users.noreply.github.com>
2024-08-14 11:31:08 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function reverse(n) {
-    
-    const reversedString = Math.abs(n).toString().split('').reverse().join('');
-    
-
-    const reversedNumber = parseInt(reversedString);
-
-    return reversedNumber;
-};
